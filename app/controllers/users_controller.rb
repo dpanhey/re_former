520fe7f4f275c4739036139acb8a710fb204d4d0
dpanhey <d.panhey@gmx.de>
2023-08-28 17:35:42 +0200

    changed method and form to form_with and added edit and update methods and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,31 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
 
   def create
-    @user = User.new(username: :username, email: :email, password: :password)
+    # @user = User.new(username: :username, email: :email, password: :password)
     # @user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
-    # @user = User.new(user_params)
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 end
