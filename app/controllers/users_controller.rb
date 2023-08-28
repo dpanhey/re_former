270856671c4b50f5b17ae34f1b7a9dadf068aef7
dpanhey <d.panhey@gmx.de>
2023-08-28 10:20:30 +0200

    added old fashioned form, added create method funtionality, created a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    @user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
