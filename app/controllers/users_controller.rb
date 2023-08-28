d6588e162151ffde4d41e14e0f514053d51d3522
dpanhey <d.panhey@gmx.de>
2023-08-28 10:24:54 +0200

    implemented private method for permiting user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
   def new; end
 
   def create
-    @user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
+    # @user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
@@ -10,4 +11,10 @@ class UsersController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:username, :email, :password)
 end
